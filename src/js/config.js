ab116d72433a3312dd3cf813af344a7553e44d54
AleLeonowicz <aleks.leonowicz@gmail.com>
2022-02-15 12:30:50 +0100

    add new recipe data upload

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,4 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = 'b91fca28-933c-4abe-a70d-ab8939b0088c';
