794ba2609af5594feedb331101dea3c55be6509c
AleLeonowicz <aleks.leonowicz@gmail.com>
2022-02-15 12:43:00 +0100

    Add recipe - new recipe rendering

--- src/js/config.js
+++ src/js/config.js
@@ -2,3 +2,4 @@ export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
 export const KEY = 'b91fca28-933c-4abe-a70d-ab8939b0088c';
+export const MODAL_CLOSE_SEC = 2.5;
